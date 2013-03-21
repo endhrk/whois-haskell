ea008231eebe138da297e8e17d2549a788eadc1a
endhrk <endhrk@gmail.com>
2013-03-21 19:39:31 +0900

    Change return value from lazy to strict.

--- Network/Whois.hs
+++ Network/Whois.hs
@@ -5,11 +5,9 @@ module Network.Whois
 import Network
 import System.IO
 import Codec.Text.IConv (convert)
-import Data.ByteString.Lazy.Char8 as BS (hGetContents,ByteString)
+import qualified Data.ByteString.Lazy.Char8 as LBS
+import qualified Data.ByteString as BS (ByteString)
 import Control.Applicative
-
-convertCode :: String -> ByteString -> ByteString
-convertCode code src = convert code "UTF-8" src
 
 getWhoisServer :: String -> String
 getWhoisServer _ = "192.41.192.40"
@@ -17,10 +15,11 @@ getWhoisServer _ = "192.41.192.40"
 getWhoisCharCode :: String -> String
 getWhoisCharCode _ = "ISO-2022-JP"
 
-whois :: String -> IO ByteString
+whois :: String -> IO BS.ByteString
 whois ip = withSocketsDo $ do
     hSetBuffering stdout NoBuffering
     h <- connectTo (getWhoisServer ip) (PortNumber 43)
     hSetBuffering h LineBuffering
     hPutStrLn h ip
-    convertCode (getWhoisCharCode ip) <$> BS.hGetContents h
+    c <- LBS.hGetContents h
+    return $ LBS.toStrict $ convert (getWhoisCharCode ip) "UTF-8" c
